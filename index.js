88e40090e71c9cbc4645c54831ace3abb00e3aa9
Mostafiz1257 <mostafiz1257@gmail.com>
2023-12-13 23:02:00 +0600

    implement save user to database

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express")
+const app = express();
+require("dotenv").config();
+const cors = require("cors")
+const port = process.env.PORT || 5000;
+
+//middleware
+const corsOptions = {
+    origin: "*",
+    credential: true,
+    optionSuccessStatus: 200,
+
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+
+//MongoDb connection
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.esni35a.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("AirCnC").collection("users")
+
+
+        app.put('/users/:email', async(req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const query = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            }
+            const result =await userCollection.updateOne(query, updateDoc, options)
+            console.log(result);
+            res.send(result);
+        })
+
+        // Connect the client to the server	(optional starting in v4.7)
+       
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send("AirCnC server is running")
+})
+app.listen(port, () => {
+    console.log(`AirCnC server is running is port ${port}`);
+})
